a3ed415fc39db6368874182f419bd11913c6456e
Demis Bellot <demis.bellot@gmail.com>
2017-01-15 01:20:41 -0500

    Add ServiceFabric Hello World ServiceStack Example

---
+++ src/StatelessSelfHost/AppHost.cs
@@ -0,0 +1,34 @@
+﻿using Funq;
+using ServiceStack;
+
+namespace StatelessSelfHost
+{
+    [Route("/hello/")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+
+    public class AppHost : AppSelfHostBase
+    {
+        public AppHost() : base("ServiceFabric SelfHost", typeof(MyServices).Assembly) {}
+
+        public override void Configure(Container container)
+        {
+        }
+    }
+}
---
+++ src/StatelessSelfHost/SelfHostCommunicationListener.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Fabric;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using ServiceStack;
+
+namespace StatelessSelfHost
+{
+    internal class SelfHostCommunicationListener : ICommunicationListener
+    {
+        private readonly ServiceEventSource eventSource;
+        private readonly ServiceContext serviceContext;
+        private readonly string endpointName;
+        private readonly string appRoot;
+
+        private AppSelfHostBase appHost;
+        private IDisposable webApp;
+        private string publishAddress;
+        private string listeningAddress;
+
+        public SelfHostCommunicationListener(AppSelfHostBase appHost, ServiceContext serviceContext, ServiceEventSource eventSource, string endpointName, string appRoot = null)
+        {
+            if (appHost == null)
+                throw new ArgumentNullException(nameof(appHost));
+
+            if (serviceContext == null)
+                throw new ArgumentNullException(nameof(serviceContext));
+
+            if (endpointName == null)
+                throw new ArgumentNullException(nameof(endpointName));
+
+            if (eventSource == null)
+                throw new ArgumentNullException(nameof(eventSource));
+
+            this.appHost = appHost;
+            this.serviceContext = serviceContext;
+            this.endpointName = endpointName;
+            this.eventSource = eventSource;
+            this.appRoot = appRoot;
+        }
+
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            var serviceEndpoint = this.serviceContext.CodePackageActivationContext.GetEndpoint(this.endpointName);
+            var protocol = serviceEndpoint.Protocol;
+            int port = serviceEndpoint.Port;
+
+            if (this.serviceContext is StatefulServiceContext)
+            {
+                StatefulServiceContext statefulServiceContext = this.serviceContext as StatefulServiceContext;
+
+                this.listeningAddress = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0}://+:{1}/{2}{3}/{4}/{5}",
+                    protocol,
+                    port,
+                    string.IsNullOrWhiteSpace(this.appRoot)
+                        ? string.Empty
+                        : this.appRoot.TrimEnd('/') + '/',
+                    statefulServiceContext.PartitionId,
+                    statefulServiceContext.ReplicaId,
+                    Guid.NewGuid());
+            }
+            else if (this.serviceContext is StatelessServiceContext)
+            {
+                this.listeningAddress = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0}://+:{1}/{2}",
+                    protocol,
+                    port,
+                    string.IsNullOrWhiteSpace(this.appRoot)
+                        ? string.Empty
+                        : this.appRoot.TrimEnd('/') + '/');
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+            this.publishAddress = this.listeningAddress.Replace("+", FabricRuntime.GetNodeContext().IPAddressOrFQDN);
+
+            try
+            {
+                this.eventSource.Message("Starting web server on " + this.listeningAddress);
+
+                //this.webApp = WebApp.Start(this.listeningAddress, appBuilder => this.startup.Invoke(appBuilder));
+                this.webApp = appHost.Init().Start(this.listeningAddress);
+
+                this.eventSource.Message("Listening on: " + this.publishAddress);
+
+                return Task.FromResult(this.publishAddress);
+            }
+            catch (Exception ex)
+            {
+                this.eventSource.Message("Web server failed to open endpoint {0}. {1}", this.endpointName, ex.ToString());
+
+                this.StopWebServer();
+
+                throw;
+            }
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            this.eventSource.Message("Closing web server on endpoint {0}", this.endpointName);
+
+            this.StopWebServer();
+
+            return Task.FromResult(true);
+        }
+
+        public void Abort()
+        {
+            this.eventSource.Message("Aborting web server on endpoint {0}", this.endpointName);
+
+            this.StopWebServer();
+        }
+
+        private void StopWebServer()
+        {
+            if (this.webApp != null)
+            {
+                try
+                {
+                    this.webApp.Dispose();
+                }
+                catch (ObjectDisposedException)
+                {
+                    // no-op
+                }
+            }
+        }
+    }
+}
